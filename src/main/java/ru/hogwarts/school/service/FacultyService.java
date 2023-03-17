818113859e126664ca377ebc1df7b93ea0fd7631
Kirill <K.katyshev1503@gmail.com>
2023-03-17 17:05:44 +0300

    Commit

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,34 +1,13 @@
 package ru.hogwarts.school.service;
-//
-//
-//import org.springframework.stereotype.Service;
-//import ru.hogwarts.school.model.Faculty;
-//import ru.hogwarts.school.model.Student;
-//
-//import java.util.HashMap;
-//
-//@Service
-//public class FacultyService {
-//
-//    private final HashMap<Long, Faculty> facultyHashMap = new HashMap<>();
-//
-//    private long lastId = 0;
-//
-//    public Faculty getFaculty (Long id) {
-//        return facultyHashMap.get(id);
-//    }
-//
-//    public Faculty postFaculty (Faculty faculty) {
-//        faculty.setId(++lastId);
-//        facultyHashMap.put(lastId, faculty);
-//        return faculty;
-//    }
-//
-//    public Faculty putFaculty (Faculty faculty) {
-//        return facultyHashMap.put(faculty.getId(), faculty);
-//    }
-//
-//    public Faculty deleteFaculty (Long id) {
-//        return facultyHashMap.remove(id);
-//    }
-//}
+
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+import java.util.HashMap;
+
+@Service
+public class FacultyService {
+
+}
